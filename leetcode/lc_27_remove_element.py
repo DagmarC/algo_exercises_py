0ac7153bd4defa4bc624960b9bbc302594ecbf1f
DagmarMac <dg,cervenkova@gmail.com>
2024-04-19 21:34:19 +0200

    copilor used

--- leetcode/lc_27_remove_element.py
+++ leetcode/lc_27_remove_element.py
@@ -4,21 +4,25 @@ from typing import List
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         """
-        Removes all occurrences of a specified value from a list of integers.
-
-        Args:
-            nums (List[int]): The list of integers.
-            val (int): The value to be removed.
-
-        Returns:
-            int: The number of elements in the modified list after removing the specified value.
-
-        Examples:
-            >>> s = Solution()
-            >>> s.removeElement([3, 2, 2, 3], 3)
-            2
-            >>> s.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)
-            5
+        This function removes all occurrences of a specified value from a list
+        of integers in-place and
+        returns the new length of the list.
+        
+        :param nums: The `nums` parameter is a list of integers that may
+        contain the value `val` that
+        needs to be removed. The function `removeElement` takes in this list
+        `nums` and the value `val`
+        that needs to be removed from the list. The function then modifies the
+        list in-place
+        :type nums: List[int]
+        :param val: The `val` parameter in the `removeElement` function
+        represents the value that you
+        want to remove from the list `nums`. The function will remove
+        all occurrences of this value from
+        the list and return the new length of the list after removal
+        :type val: int
+        :return: the new length of the array `nums` after removing all
+        occurrences of the value `val`.
         """
         k = len(nums) - 1
         for i in range(len(nums) - 1, -1, -1):
