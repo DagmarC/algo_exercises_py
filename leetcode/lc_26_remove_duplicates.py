fb56fcc5b9f7051f3911f4f7165f7af7d9b66bb2
DagmarMac <dg,cervenkova@gmail.com>
2024-04-20 15:06:04 +0200

    remove duplicates

---
+++ leetcode/lc_26_remove_duplicates.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def remove_duplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        write_pointer = 1
+        for read_pointer in range(1, len(nums)):
+            if nums[read_pointer] != nums[read_pointer - 1]:
+                nums[write_pointer] = nums[read_pointer]
+                write_pointer += 1
+
+        return write_pointer
