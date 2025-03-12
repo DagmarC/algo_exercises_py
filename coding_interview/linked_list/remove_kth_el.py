59e9ec7fdc9461fb03dcc84972a70c97f1de9a6b
DagmarMac <dg,cervenkova@gmail.com>
2025-03-12 13:42:14 +0100

    CI, Linked list, remove kth last element

--- coding_interview/linked_list/remove_kth_el.py
+++ coding_interview/linked_list/remove_kth_el.py
@@ -5,7 +5,6 @@ class ListNode:
 
 
 def remove_kth_last_node(head: ListNode, k: int) -> ListNode:
-    # Write your code here
 
     def count_nodes() -> int:
         n = head
@@ -20,13 +19,12 @@ def remove_kth_last_node(head: ListNode, k: int) -> ListNode:
     curr_node = head
     prev = None
     i = 0
-    
     while curr_node:
         if count-k == i and curr_node == head:
-            head = curr_node.next
-            
+            head = curr_node.next 
         if count-k == i and prev:
             prev.next = curr_node.next
+            
         prev = curr_node
         curr_node = curr_node.next
         i += 1
