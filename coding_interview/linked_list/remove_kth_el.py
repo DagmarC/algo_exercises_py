44338435d86b3ef7bbcb25c0b3f4ae50d3a2f37e
DagmarMac <dg,cervenkova@gmail.com>
2025-03-13 17:54:39 +0100

    CI, Linked list, remove kth element from the end, 2ptr solution

--- coding_interview/linked_list/remove_kth_el.py
+++ coding_interview/linked_list/remove_kth_el.py
@@ -3,8 +3,38 @@ class ListNode:
         self.val = val
         self.next = next
 
+""" 
+ remove_kth_last_node: 2 pointer solutions
+ -> create the temp node to point to HEAD and set both trailer and leader pointer to it (in case head to be deleted)
+ -> firstly move leader and when space between trailer and leader reaches k, then move trailer
+ -> when leader.next is None (the end of the linked list) then the trailer pointer 
+    should correctly point tho the node right before the node to be deleted - node to be deleted is the trailer.nex
+"""
+def remove_kth_last_node(head: ListNode, k: int) -> ListNode: 
+    first_node = ListNode()  # 1st node to make possible head removal if needed
+    first_node.next = head
+    
+    trailer_node, leader_node = first_node, first_node 
+    space = 0  # move the trailing pointer after the space between the leader and the trailer is k
+    while leader_node.next:
+        if space < k:
+            leader_node = leader_node.next
+            space += 1 
+            continue
+        leader_node = leader_node.next
+        trailer_node = trailer_node.next
+    
+    # Note if space != k then there is no such node to be deleted, k is too large    
+    if trailer_node.next == head and space == k:
+        head = head.next
+    # When the leader.next is None, you reached the end of the linked list and the trailer pointer 
+    # should correctly point tho the node right before the node to be deleted - node to be deleted is the trailer.next
+    trailer_node.next = trailer_node.next.next
+    
+    return head
 
-def remove_kth_last_node(head: ListNode, k: int) -> ListNode:
+
+def remove_kth_last_node_naive(head: ListNode, k: int) -> ListNode:
 
     def count_nodes() -> int:
         n = head
