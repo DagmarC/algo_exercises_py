f8634334e4aa1de7751edb36448842dce246d1b1
DagmarMac <dg,cervenkova@gmail.com>
2025-05-07 13:51:24 +0200

    CI, LI, intersection and palindrome improvement, more methods added

--- coding_interview/linked_list/intersection.py
+++ coding_interview/linked_list/intersection.py
@@ -8,24 +8,8 @@ def linked_list_intersection(head_A: ListNode, head_B: ListNode):
     n1 = head_A
     n2 = head_B
     
-    inter_node = None
-    while n1 and n2:
-        if n1 == n2:
-            break
-            
-        if n1.next == n2:
-            n1 = n1.next
-        elif n2.next == n1:
-            n2 = n2.next
-        else:
-            n1 = n1.next
-            n2 = n2.next  
-    
-    # At the point of intersection, at the end both must be None
-    while n1 and n2 and n1 == n2:
-        if not inter_node:
-            inter_node = n1
-        n1 = n1.next
-        n2 = n2.next
-            
-    return inter_node if (not n1) and (not n2) else None
+    # both pointers traverse m + n steps and align perfectly at the intersection or both reach None at the same time if there's no intersection.
+    while n1 != n2:
+        n1 = n1.next if n1 else head_B
+        n2 = n2.next if n2 else head_A 
+    return n1
--- coding_interview/linked_list/palindrome.py
+++ coding_interview/linked_list/palindrome.py
@@ -3,7 +3,10 @@ class ListNode:
         self.val = val
         self.next = next
 
-def palindromic_linked_list(head: ListNode) -> bool:
+############## 1st solution ##############
+##########################################
+
+def palindromic_linked_list_two_lists(head: ListNode) -> bool:
     # Write your code here
     # tmpNode = ListNode(val="<Start>", next=head)  # point to the head and set ptrs to the tmp pointer
     p1, p2 = head, head
@@ -30,6 +33,76 @@ def palindromic_linked_list(head: ListNode) -> bool:
     print(first_part, second_part, second_part[::-1])
     return first_part == second_part[::-1]
 
+############## 2nd solution ##############
+##########################################
+
+def palindromic_linked_list_stack(head: ListNode) -> bool:
+    # Write your code here
+    # tmpNode = ListNode(val="<Start>", next=head)  # point to the head and set ptrs to the tmp pointer
+    p1, p2 = head, head
+    
+    # Approach: p2 is 2x faster than p1, when p2 reaches the end, p1 is in the middle 
+    # -> compare the rest of p1.next - end  with the string that has been captured with p1 [start - middle]
+    first_part = []  # stack
+    while p2 and p2.next:
+        first_part.append(p1.val)
+        p1 = p1.next
+        p2 = p2.next.next
+        
+    if p1 == p2:
+        return True  # only head in the Linked List
+    
+    # Odd number of nodes, skip the middle pointer  a->b->a->None (p1 points to b, p2 points to a, where a.next = None)
+    if p2:
+        p1 = p1.next 
+        
+    while p1:
+        el = first_part.pop()
+        if el != p1.val:
+            return False
+        p1 = p1.next
+    
+    return True
+
+############## 3rd solution ##############
+##########################################
+class MatchState:
+    def __init__(self):
+        self.is_match = True  # boolean value is immutable, so you need a wrapper class to update the match state
+
+def palindromic_linked_list(head: ListNode):
+    # If somewhere in the recutrsion call there was not a match, it is set to False, otherwise stays True --> palindrome
+    match = MatchState()
+    _palindromic_linked_list_rec(head, palindromic_linked_list_len(head), match)
+    return match.is_match
+
+def _palindromic_linked_list_rec(head: ListNode, length: int, match: MatchState):
+    if not head or length == 0:  # Even number of nodes
+        return head
+    if length == 1:  # Odd number of nodes (skipt the middle)
+        return head.next
+    
+    result_node = _palindromic_linked_list_rec(head.next, length-2, match)
+    
+    # Early exit if mismatch has already been found
+    if not match.is_match or not result_node:
+        return result_node 
+    
+    # Compare current head and result_node values
+    if head.val != result_node.val:
+        match.is_match = False
+    
+    return result_node.next
+
+def palindromic_linked_list_len(node: ListNode) -> int:
+    """Returns the lenght of the linked list"""
+    length = 0
+    while node:
+        length += 1
+        node = node.next
+    return length
+
+
 def main():
     # Create linked list: 1 -> 2 -> 3 -> 2 -> 1
     nodes = [ListNode(1), ListNode(2), ListNode(3), ListNode(2), ListNode(1)]
@@ -37,10 +110,10 @@ def main():
         nodes[i].next = nodes[i + 1]
     head = nodes[0]
 
-    result = palindromic_linked_list(head)
+    result = palindromic_linked_list_stack(head)
     print("Is palindrome:", result)
     
-    nodes = [ListNode(1), ListNode(2), ListNode(2), ListNode(1)]
+    nodes = [ListNode(1), ListNode(2), ListNode(3), ListNode(1)]
     for i in range(len(nodes) - 1):
         nodes[i].next = nodes[i + 1]
     head = nodes[0]
